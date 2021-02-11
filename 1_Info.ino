eae4d1431275a7d1a95b48a4a9b71efc339aa03d
khirds <66429663+khirds@users.noreply.github.com>
2021-02-11 15:49:31 +0500

    Add files via upload

---
+++ 1_Info.ino
@@ -0,0 +1,74 @@
+/* https://github.com/khirds/powertalkz
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * https://github.com/programmer131/ESP8266_ESP32_SelfUpdate/issues/8
+ * Thanks
+Based on the timemage answer via the other question, I have changed the address of bin and TXT file by finding the download link of those files form the GitHub. And it has been working by changing the blob part of address to raw, so the bin address changed form
+https://github.com/soheilpaper/ota_github_blink/blob/main/BlinkWithoutDelay_ino_esp8285.bin to
+https://github.com/soheilpaper/ota_github_blink/raw/main/BlinkWithoutDelay_ino_esp8285.bin And the 
+https://github.com/soheilpaper/ota_github_blink/blob/main/version.txt
+to
+https://raw.githubusercontent.com/soheilpaper/ota_github_blink/main/version.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rough
+//#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
+//#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+
+//#define URL_fw_Version "https://github.com/khirds/powertalkz/blob/khirds-patch-2/bin_version.txt"
+//#define URL_fw_Bin "https://github.com/khirds/powertalkz/blob/khirds-patch-2/fw.bin"
+
+//
+//https://github.com/soheilpaper/ota_github_blink/blob/main/BlinkWithoutDelay_ino_esp8285.bin to
+//https://github.com/soheilpaper/ota_github_blink/raw/main/BlinkWithoutDelay_ino_esp8285.bin And the 
+//https://github.com/soheilpaper/ota_github_blink/blob/main/version.txt
+//to
+//https://raw.githubusercontent.com/soheilpaper/ota_github_blink/main/version.txt
+
+//#define URL_fw_Bin "https://github.com/khirds/powertalkz/raw/main/fw.bin"
+//#define URL_fw_Version "https://raw.github.com/khirds/powertalkz/main/bin_version.txt"
+
+//raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
+//#define URL_fw_Bin "https://github.com/khirds/powertalkz/raw/main/bin_version.txt"
+//#define URL_fw_Version "https://raw.githubusercontent.com/khirds/powertalkz/main/bin_version.txt"
+
+//#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
+//#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
+
+//#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
+//#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+
+https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin
+https://github.com/khirds/powertalkz/blob/main/fw.bin
+
+https://raw.githubusercontent.com/khirds/powertalkz/main/bin_version.txt
+https://raw.githubusercontent.com/khirds/powertalkz/main/fw.bin
+
+https://raw.githubusercontent.com/khirds/powertalkz/blob/main/fw.bin
+https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt
+
+
+#define URL_fw_Bin "https://github.com/khirds/powertalkz/blob/main/fw.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/khirds/powertalkz/main/bin_version.txt"
+ */
---
+++ PT_FOTA_PO_V2.ino
@@ -0,0 +1,103 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
+#include <WiFiClientSecure.h>
+#include "cert.h"
+
+//const char * ssid = "home_wifi";
+//const char * password = "helloworld";
+const char * ssid = "PC";
+const char * password = "idhh1925";
+
+
+//Fota Deatals for GIT--------------
+String FirmwareVer = {
+  "2.6"
+};
+
+#define URL_fw_Version "https://raw.githubusercontent.com/khirds/powertalkz/main/bin_version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/khirds/powertalkz/main/fw.bin"
+
+//Fota details end---------------------------------------
+
+void connect_wifi();
+void firmwareUpdate();
+int FirmwareVersionCheck();
+
+unsigned long previousMillis = 0; // will store last time LED was updated
+unsigned long previousMillis_2 = 0;
+const long interval = 60000;
+const long mini_interval = 1000;
+void repeatedCall() {
+  static int num=0;
+  unsigned long currentMillis = millis();
+  if ((currentMillis - previousMillis) >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    if (FirmwareVersionCheck()) {
+      firmwareUpdate();
+    }
+  }
+  if ((currentMillis - previousMillis_2) >= mini_interval) {
+    previousMillis_2 = currentMillis;
+    Serial.print("idle loop...");
+    Serial.print(num++);
+    Serial.print(" Active fw version:");
+    Serial.println(FirmwareVer);
+   if(WiFi.status() == WL_CONNECTED) 
+   {
+       Serial.println("wifi connected");
+   }
+   else
+   {
+    connect_wifi();
+   }
+  }
+}
+
+struct Button {
+  const uint8_t PIN;
+  uint32_t numberKeyPresses;
+  bool pressed;
+};
+
+Button button_boot = {
+  0,
+  0,
+  false
+};
+/*void IRAM_ATTR isr(void* arg) {
+    Button* s = static_cast<Button*>(arg);
+    s->numberKeyPresses += 1;
+    s->pressed = true;
+}*/
+
+void IRAM_ATTR isr() {
+  button_boot.numberKeyPresses += 1;
+  button_boot.pressed = true;
+}
+
+
+void setup() {
+  pinMode(button_boot.PIN, INPUT);
+  attachInterrupt(button_boot.PIN, isr, RISING);
+  Serial.begin(115200);
+  Serial.print("Active firmware version:");
+  Serial.println(FirmwareVer);
+  pinMode(LED_BUILTIN, OUTPUT);
+  connect_wifi();
+  Serial.println(URL_fw_Version);
+  Serial.println(URL_fw_Bin);
+//  #define URL_fw_Version "https://raw.githubusercontent.com/khirds/powertalkz/main/bin_version.txt"
+//#define URL_fw_Bin "https://raw.githubusercontent.com/khirds/powertalkz/main/fw.bin"
+}
+void loop() {
+  if (button_boot.pressed) { //to connect wifi via Android esp touch app 
+    Serial.println("Firmware update Starting..");
+    firmwareUpdate();
+    button_boot.pressed = false;
+  }
+  repeatedCall();
+}
+
+
